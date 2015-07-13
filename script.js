4b5af850420d7796dc4b004e179b4e5c3594cd2b
Cris <rhapsodyshadow@yahoo.com>
2015-07-12 22:47:26 -0700

    assigned hover styles to clickers and added detail to class and id

---
+++ script.js
@@ -0,0 +1,16 @@
+//////////   RED CLICKER  /////////////////////////////////
+function red(){
+$("#red").on("click", function() {
+    $("#1").toggleClass("red");
+});
+}
+
+red();
+///////////////////   ORANGE CLICKER   ///////////////////
+function orange(){
+$("#orange").on("click", function() {
+    $("#2").toggleClass("orange");
+});
+}
+
+orange();
